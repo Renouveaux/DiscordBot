8a8fd264e71fface8e8e8d8d5af9f0aa877094a2
Jonathan Boyer <jonathanboyer@MacBook-Pro.local>
2015-09-06 05:40:50 +0200

    Mise en place de chai

--- test/test.js
+++ test/test.js
@@ -1,17 +1,19 @@
-require("should");
+var expect = require("chai").expect
+    , CodeFilter = require('../lib/filters/code/code');
 
-describe('CodeFilter', function() {
-    CodeFilter = require('../lib/filters/code/code')
+describe('filters/code', function() {
+
     describe('#isCode()', function () {
-        it('should let not code pass', function () {
-            CodeFilter.isCode('Salut les gens' ).should.not.be.ok()
+        it('should let standart text pass', function () {
+            expect( CodeFilter.isCode('Salut les gens' ) ).to.not.be.ok;
         });
         it('should let code pass on one line', function () {
-            CodeFilter.isCode('vérifie les {}').should.not.be.ok()
+            expect( CodeFilter.isCode('vérifie les {} je pense que ça vient de là') ).to.not.be.ok;
         });
-        it('should detect code based on {}', function () {
-            CodeFilter.isCode('var assert = require("assert");\ndescribe(\'CodeIntercaptor\', function() {\n    describe(\'#intercep()\', function () {\n        it(\'should return -1 when the value is not present\', function () {\n            assert.equal(-1, [1,2,3].indexOf(5));\n            assert.equal(-1, [1,2,3].indexOf(0));\n        });\n    });\n});')
-                .should.be.ok()
+        it('should detect code based on }', function () {
+            expect(
+                CodeFilter.isCode('var assert = require("assert");\ndescribe(\'CodeIntercaptor\', function() {\n    describe(\'#intercep()\', function () {\n        it(\'should return -1 when the value is not present\', function () {\n            assert.equal(-1, [1,2,3].indexOf(5));\n            assert.equal(-1, [1,2,3].indexOf(0));\n        });\n    });\n});')
+            ).to.be.ok;
         });
     });
 });
